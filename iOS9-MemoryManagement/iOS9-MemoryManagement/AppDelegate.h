b8c1796cd5857d3a766c7f36f4744a46d085e635
Paul Solt <PaulSolt@gmail.com>
2019-11-13 12:36:37 -0500

    Starter project

---
+++ iOS9-MemoryManagement/iOS9-MemoryManagement/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS9-MemoryManagement
+//
+//  Created by Paul Solt on 11/13/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS9-MemoryManagement/iOS9-MemoryManagement/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  iOS9-MemoryManagement
+//
+//  Created by Paul Solt on 11/13/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+
+
+
+
+}
+
+
+@end
